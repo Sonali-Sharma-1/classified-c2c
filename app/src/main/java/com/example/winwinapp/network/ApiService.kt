7540f25efb7def18679bbdce6a0a20db0ce37946
Sonali Sharma <sosharma@falabella.cl>
2021-11-17 12:43:45 +0530

    ui changes for login signup

--- app/src/main/java/com/example/winwinapp/network/ApiService.kt
+++ app/src/main/java/com/example/winwinapp/network/ApiService.kt
@@ -3,9 +3,6 @@ package com.example.winwinapp.network
 import com.example.winwinapp.data.dataclasses.LoginBody
 import com.example.winwinapp.data.dataclasses.LoginResponse
 import com.example.winwinapp.data.dataclasses.SignUpBody
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.squareup.okhttp.Response
 import com.squareup.okhttp.logging.HttpLoggingInterceptor
 import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
@@ -17,33 +14,33 @@ import retrofit2.http.*
 interface ApiService {
     @Headers("Content-Type:application/json")
 
-    @POST("auth-token")
+    @POST("user/login")
     fun login(@Body info: LoginBody): Call<ResponseBody>
 
-    @POST("users")
+    @POST("user/create")
     fun registerUser(@Body info: SignUpBody): Call<LoginResponse>
 
 }
 
 class RetrofitInstance {
 
-    companion object {
-        private const val BASE_URL: String = "https://jsonplaceholder.typicode.com/"
-
-        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
-            this.level = HttpLoggingInterceptor.Level.BODY
-        }
-
-        val client: OkHttpClient = OkHttpClient.Builder().apply {
-            this.addInterceptor(interceptor)
-        }.build()
-
-        fun getRetrofitInstance(): Retrofit {
-            return Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .client(client)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        }
-    }
+//    companion object {
+//        private const val BASE_URL: String = "http://localhost:8080/"
+//
+//        val interceptor: HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
+//            this.level = HttpLoggingInterceptor.Level.BODY
+//        }
+//
+//        val client: OkHttpClient = OkHttpClient.Builder().apply {
+//            this.addInterceptor(interceptor)
+//        }.build()
+//
+//        fun getRetrofitInstance(): Retrofit {
+//            return Retrofit.Builder()
+//                .baseUrl(BASE_URL)
+//                .client(client)
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build()
+//        }
+//    }
 }
