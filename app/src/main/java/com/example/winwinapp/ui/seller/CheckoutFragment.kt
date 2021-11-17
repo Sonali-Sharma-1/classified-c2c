4cfce9f53df4177e26d34f838700abbe6be5617b
Sonali Sharma <sosharma@falabella.cl>
2021-11-17 19:34:25 +0530

    minor changes

--- app/src/main/java/com/example/winwinapp/ui/seller/CheckoutFragment.kt
+++ app/src/main/java/com/example/winwinapp/ui/seller/CheckoutFragment.kt
@@ -1,14 +1,20 @@
 package com.example.winwinapp.ui.seller
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.example.winwinapp.data.dataclasses.Bidding
+import com.example.winwinapp.data.dataclasses.MockData
+import com.example.winwinapp.data.dataclasses.ProductX
 import com.example.winwinapp.databinding.FragmentCheckoutBinding
 
 class CheckoutFragment : Fragment() {
 
+    private var mainList = mutableListOf<ProductX>()
     private lateinit var binding: FragmentCheckoutBinding
     private val hashMap = HashMap<String, String>()
 
@@ -25,6 +31,21 @@ class CheckoutFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        mainList = MockData.dataForForProductCatalog() as MutableList<ProductX>
+        val product = mainList[0]
+        context?.let {
+            Glide.with(it)
+                .load(Uri.parse(product.images[0]))
+                .into(binding.imgProduct)
+        }
+
+        binding.tvName.text =  product.name
+        val biddingList = Bidding("sonali", 290000)
+        binding.tvBidPrice.text = "Your Bid"
+        binding.tvSubtotal.text = "$29.109.00"
+        binding.tvTotal.text = "$500.000"
+        binding.totalCmr.text = "$460.00"
     }
 
 
